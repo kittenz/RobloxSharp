fb2b2257a62d32820df8851d5413a6cb658fab9a
Roni Nevalainen <kitten@kittenz.dev>
2021-02-23 20:54:56 +0200

    Make Program.cs work with nodes

--- src/Compiler/Program.cs
+++ src/Compiler/Program.cs
@@ -1,10 +1,57 @@
 ﻿using System;
 using System.IO;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+using RobloxSharp.Compiler;
 
 namespace RobloxSharp.Compiler {
 	class Program {
+		string luaOutput = "-- Compiled with RobloxSharp\n";
+
 		void ParseSourceFile(string source) {
-			Console.WriteLine(source);
+			SyntaxTree tree = CSharpSyntaxTree.ParseText(source);
+			CompilationUnitSyntax root = tree.GetCompilationUnitRoot();
+
+			foreach (var usingDirective in root.Usings) {
+				ParseUsing(usingDirective);
+			}
+
+			foreach (MemberDeclarationSyntax member in root.Members) {
+				CSharpSyntaxNode node = (CSharpSyntaxNode)member;
+				
+				ParseNode(node);
+			}
+		}
+
+		void ParseUsing(UsingDirectiveSyntax usingDirective) {
+			
+		}
+
+		void ParseNode(CSharpSyntaxNode node) {
+			switch (node.Kind()) {
+				case SyntaxKind.VariableDeclaration: {
+					var syntax = (VariableDeclarationSyntax)node;			
+					Nodes.VariableNode varNode = new Nodes.VariableNode(syntax);
+
+					AddToLuaOutput($"{varNode.ToLua()}\n");
+
+					break;
+				}
+			}
+
+			foreach (var childNode in node.ChildNodes()) {
+				CSharpSyntaxNode csharpNode = (CSharpSyntaxNode)childNode;
+				ParseNode(csharpNode);
+			}
+		}
+
+		void AddToLuaOutput(string output) {
+			luaOutput += output;
+		}
+
+		void OutputLua() {
+			Console.WriteLine(luaOutput);
 		}
 
 		static void Main(string[] args) {
@@ -23,6 +70,8 @@ namespace RobloxSharp.Compiler {
 				var reader = new StreamReader(filename);
 
 				program.ParseSourceFile(reader.ReadToEnd());
+
+				program.OutputLua();
 			} catch (IOException e) {
 				Console.WriteLine($"[ERROR]: The file {filename} cannot be read: {e.Message}");
 			}
