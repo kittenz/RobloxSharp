23a0f9771c18d6b591dcdaa0595f6fd2501af4ed
Roni Nevalainen <kitten@kittenz.dev>
2021-02-24 15:46:52 +0200

    compiler: move compiler code to its own class
    The compiler code looked pretty ugly in the main program class so I
    moved it into it's own file/class.

--- src/Compiler/Compiler.cs
+++ src/Compiler/Compiler.cs
@@ -1,1 +1,64 @@
-// TODO: Compiler class
+using System;
+using System.IO;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+
+namespace RobloxSharp.Compiler {
+	class Compiler {
+		private string _luaOutput = "-- Compiled with RobloxSharp\n";
+		private string _sourceCode = "";
+
+		public string LuaOutput { get => _luaOutput; }
+
+		public Compiler(string sourceFile) {
+			if (!File.Exists(sourceFile)) {
+				Console.WriteLine($"[ERROR]: The file {sourceFile} cannot be found");
+				Environment.Exit(1);
+			} else if (Path.GetExtension(sourceFile) != ".cs") {
+				Console.WriteLine($"[ERROR]: The file {sourceFile} isn't a C# source file");
+				Environment.Exit(2);
+			}
+
+			try {
+				var reader = new StreamReader(sourceFile);
+
+				_sourceCode = reader.ReadToEnd();
+			} catch (Exception e) {
+				Console.WriteLine($"[ERROR]: The file {sourceFile} cannot be read: {e.Message}");
+			}
+		}
+
+		private void _ParseSyntaxNode(CSharpSyntaxNode syntaxNode, bool isRecursive) {
+			switch (syntaxNode.Kind()) {
+				case SyntaxKind.VariableDeclaration: {
+					var syntax = (VariableDeclarationSyntax)syntaxNode;			
+					Nodes.VariableNode variableNode = new Nodes.VariableNode(syntax);
+
+					_luaOutput += $"{variableNode.ToLua()}\n";
+
+					break;
+				}
+			}
+
+			if (isRecursive) {
+				foreach (var childNode in syntaxNode.ChildNodes()) {
+					CSharpSyntaxNode csharpNode = (CSharpSyntaxNode)childNode;
+					
+					_ParseSyntaxNode(csharpNode, true);
+				}
+			}
+		}
+
+		public void Compile() {
+			SyntaxTree tree = CSharpSyntaxTree.ParseText(_sourceCode);
+			CompilationUnitSyntax root = tree.GetCompilationUnitRoot();
+
+			foreach (MemberDeclarationSyntax member in root.Members) {
+				CSharpSyntaxNode syntaxNode = (CSharpSyntaxNode)member;
+
+				_ParseSyntaxNode(syntaxNode, true);
+			}
+		}
+	}
+}
--- src/Compiler/Program.cs
+++ src/Compiler/Program.cs
@@ -1,80 +1,12 @@
 ﻿using System;
-using System.IO;
-using Microsoft.CodeAnalysis;
-using Microsoft.CodeAnalysis.CSharp;
-using Microsoft.CodeAnalysis.CSharp.Syntax;
-using RobloxSharp.Compiler;
 
 namespace RobloxSharp.Compiler {
 	class Program {
-		string luaOutput = "-- Compiled with RobloxSharp\n";
-
-		void ParseSourceFile(string source) {
-			SyntaxTree tree = CSharpSyntaxTree.ParseText(source);
-			CompilationUnitSyntax root = tree.GetCompilationUnitRoot();
-
-			foreach (var usingDirective in root.Usings) {
-				ParseUsing(usingDirective);
-			}
-
-			foreach (MemberDeclarationSyntax member in root.Members) {
-				CSharpSyntaxNode node = (CSharpSyntaxNode)member;
-				
-				ParseNode(node);
-			}
-		}
-
-		void ParseUsing(UsingDirectiveSyntax usingDirective) {
-			
-		}
-
-		void ParseNode(CSharpSyntaxNode node) {
-			switch (node.Kind()) {
-				case SyntaxKind.VariableDeclaration: {
-					var syntax = (VariableDeclarationSyntax)node;			
-					Nodes.VariableNode varNode = new Nodes.VariableNode(syntax);
-
-					AddToLuaOutput($"{varNode.ToLua()}\n");
-
-					break;
-				}
-			}
-
-			foreach (var childNode in node.ChildNodes()) {
-				CSharpSyntaxNode csharpNode = (CSharpSyntaxNode)childNode;
-				ParseNode(csharpNode);
-			}
-		}
-
-		void AddToLuaOutput(string output) {
-			luaOutput += output;
-		}
-
-		void OutputLua() {
-			Console.WriteLine(luaOutput);
-		}
-
 		static void Main(string[] args) {
-			var program = new Program();
-			string filename = args[0];
+			var compiler = new Compiler(args[0]);
 
-			if (!File.Exists(filename)) {
-				Console.WriteLine($"[ERROR]: The file {filename} cannot be found");
-				Environment.Exit(1);
-			} else if (Path.GetExtension(filename) != ".cs") {
-				Console.WriteLine($"[ERROR]: The file {filename} isn't a C# source file");
-				Environment.Exit(2);
-			}
-
-			try {
-				var reader = new StreamReader(filename);
-
-				program.ParseSourceFile(reader.ReadToEnd());
-
-				program.OutputLua();
-			} catch (IOException e) {
-				Console.WriteLine($"[ERROR]: The file {filename} cannot be read: {e.Message}");
-			}
+			compiler.Compile();
+			Console.WriteLine(compiler.LuaOutput);
 		}
 	}
 }
